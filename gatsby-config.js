3c8cade8539476c21a46d704cb174b1f3874e898
Lawrence <Caulker>
2020-06-03 21:48:23 -0400

    completed initial blog template

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,9 +12,9 @@ console.log(
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `GatsbyWP App`,
+    description: `A Gatsby-WordPress Application with GraphQL, CPT, ACF.`,
+    author: `Lawrence O Caulker`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
